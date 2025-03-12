02c15de302144635098f86c636fa5813bad0c094
TRJackson998 <85368064+TRJackson998@users.noreply.github.com>
2025-03-12 14:45:44 -0700

    Initial setup

---
+++ pic_cron.py
@@ -0,0 +1,13 @@
+"""
+Pic Cron
+========
+Script for the main functionality of scraping meta-data from pictures and renaming them
+Starting out with accessing files from my filesystemw
+"""
+
+def main():
+    """Driver"""
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
