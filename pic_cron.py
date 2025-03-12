e8b9c0c6cdc18641da760f3129b3a24c811f7048
TRJackson998 <85368064+TRJackson998@users.noreply.github.com>
2025-03-12 14:46:31 -0700

    Attempt 1 with PIL

--- pic_cron.py
+++ pic_cron.py
@@ -4,10 +4,33 @@ Pic Cron
 Script for the main functionality of scraping meta-data from pictures and renaming them
 Starting out with accessing files from my filesystemw
 """
+from pathlib import Path
+
+from PIL import Image, UnidentifiedImageError
+from PIL.ExifTags import TAGS
+
 
 def main():
     """Driver"""
-    print("Hello, world!")
+    
+    pic_dir = Path(r"E:\NZ Pics\JJ")
+    for pic in pic_dir.iterdir():
+        try:
+            image = Image.open(str(pic))
+        except UnidentifiedImageError:
+            continue
+        
+        # extract EXIF data
+        exif_data = image.getexif()
+
+        for tag_id in exif_data:
+            # get the tag name, instead of human unreadable tag id
+            tag = TAGS.get(tag_id, tag_id)
+            data = exif_data.get(tag_id)
+            # decode bytes 
+            if isinstance(data, bytes):
+                data = data.decode()
+            print(f"{tag:25}: {data}")
 
 if __name__ == "__main__":
     main()
